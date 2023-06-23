506755908aa78ebfc0d6e88826131835e5da8dc8
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-23 11:40:12 -0500

    create a title bar and main content area

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='window roundedBdr' >
+      <header className='roundedBdr'>
+        <h1>Hook Form</h1>
       </header>
+      <div>
+        <div className='flexRow'>
+          <div className='col1 pad10 roundedBdr'>Form</div>
+          <div className='col1 pad10 roundedBdr'>Form Info</div>
+        </div>
+      </div>
     </div>
   );
 }
